8d6f99c3dc83f956589b1f0adf64cc898ad86daf
Frank <frankedwards50@gmail.com>
2024-05-28 15:17:12 -0700

    Style Variables

---
+++ styles/variables.ts
@@ -0,0 +1,91 @@
+// theme.js
+
+export const colors = {
+  // Primary Color Shades
+  primary100: "#D6E1F6",
+  primary200: "#ADC3EC",
+  primary300: "#84A5E2",
+  primary400: "#5B87D8",
+  primary500: "#4276CF",
+  primary600: "#365EA4",
+  primary700: "#294579",
+  primary800: "#1D2D4F",
+  primary900: "#101625",
+
+  // Success Colors
+  success100: "#D6F6E4",
+  success200: "#ADF1C9",
+  success300: "#84EBAE",
+  success400: "#5BE593",
+  success500: "#32E078",
+  success600: "#29B860",
+  success700: "#208F48",
+  success800: "#166630",
+  success900: "#0D3D18",
+
+  // Warning Colors
+  warning100: "#FFF6D6",
+  warning200: "#FFEDAD",
+  warning300: "#FFE384",
+  warning400: "#FFDA5B",
+  warning500: "#FFD132",
+  warning600: "#CCAA29",
+  warning700: "#998320",
+  warning800: "#665C16",
+  warning900: "#33250D",
+
+  // Error Colors
+  error100: "#F6D6D6",
+  error200: "#ECADAD",
+  error300: "#E28484",
+  error400: "#D85B5B",
+  error500: "#CF3232",
+  error600: "#A42A2A",
+  error700: "#792122",
+  error800: "#4F1919",
+  error900: "#251010",
+
+  // Info Colors
+  info100: "#D6E6F6",
+  info200: "#ADCDEB",
+  info300: "#84B3E0",
+  info400: "#5B9AD5",
+  info500: "#3280CA",
+  info600: "#2968A3",
+  info700: "#204F7B",
+  info800: "#163754",
+  info900: "#0D1E2D",
+
+  // Neutral Colors
+  neutral100: "#f0f1f1",
+  neutral200: "#d3d4d6",
+  neutral300: "#b6b8ba",
+  neutral400: "#999c9e",
+  neutral500: "#7c7f83",
+  neutral600: "#616366",
+  neutral700: "#454749",
+  neutral800: "#292a2c",
+  neutral900: "#0e0e0f",
+
+  transparentBlack100: "rgba(neutral900,  0.1)",
+  transparentBlack200: "rgba(neutral900,  0.2)",
+  transparentBlack300: "rgba(neutral900,  0.3)",
+  transparentBlack400: "rgba(neutral900,  0.4)",
+  transparentBlack500: "rgba(neutral900,  0.5)",
+  transparentBlack600: "rgba(neutral900,  0.6)",
+  transparentBlack700: "rgba(neutral900,  0.7)",
+  transparentBlack800: "rgba(neutral900,  0.8)",
+  transparentBlack900: "rgba(neutral900,  0.9)",
+};
+
+export const fontSizes = {
+  header1: 56.8,
+  header2: 42.6,
+  header3: 32,
+  general: 24,
+  body: 18,
+};
+
+export const borderRadius = {
+  borderRadius: 5,
+};
