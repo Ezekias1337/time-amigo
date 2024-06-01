b594951d85db2f69db034307b5e239b088ee7215
Frank <frankedwards50@gmail.com>
2024-06-01 16:42:50 -0700

    keep awake, schedule, time implemented

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, AppState } from "react-native";
 import DateTimePicker, {
   DateTimePickerEvent,
 } from "@react-native-community/datetimepicker";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import * as Speech from "expo-speech";
-import { StatusBar } from 'expo-status-bar';
+import { Audio } from "expo-av";
+import { useKeepAwake } from 'expo-keep-awake';
+import { StatusBar } from "expo-status-bar";
 import Button from "../components/Button";
 import DayOfWeekSelector from "../components/DaySelector";
 import { useLocalization } from "../components/LocalizationContext";
@@ -12,9 +15,11 @@ import { colors } from "../styles/variables";
 
 const Home: React.FC = () => {
   const { translate, locale } = useLocalization();
+  useKeepAwake();
 
   const [show, setShow] = useState(false);
   const [time, setDate] = useState<Date | null>(null);
+  const [selectedDays, setSelectedDays] = useState<number[]>([]);
   const [frequency, setFrequency] = useState(1);
   const [startAnnouncingTime, setStartAnnouncingTime] = useState(false);
 
@@ -22,6 +27,62 @@ const Home: React.FC = () => {
     const currentDate = selectedDate || time;
     setDate(currentDate);
     setShow(false);
+    saveTimeToStorage(currentDate);
+  };
+
+  const saveTimeToStorage = async (selectedTime: Date | null) => {
+    try {
+      if (!selectedTime) {
+        throw Error("No time selected");
+      }
+      await AsyncStorage.setItem("announcementTime", selectedTime.toString());
+    } catch (error) {
+      console.error("Error saving time to storage", error);
+    }
+  };
+
+  const loadSettingsFromStorage = async () => {
+    try {
+      const storedTime = await AsyncStorage.getItem("announcementTime");
+      let storedDays: number[] = [];
+
+      if (storedTime) {
+        setDate(new Date(storedTime));
+      }
+
+      let sunday = await AsyncStorage.getItem("sunday");
+      let monday = await AsyncStorage.getItem("monday");
+      let tuesday = await AsyncStorage.getItem("tuesday");
+      let wednesday = await AsyncStorage.getItem("wednesday");
+      let thursday = await AsyncStorage.getItem("thursday");
+      let friday = await AsyncStorage.getItem("friday");
+      let saturday = await AsyncStorage.getItem("saturday");
+
+      if (sunday === "true") {
+        storedDays.push(0);
+      }
+      if (monday === "true") {
+        storedDays.push(1);
+      }
+      if (tuesday === "true") {
+        storedDays.push(2);
+      }
+      if (wednesday === "true") {
+        storedDays.push(3);
+      }
+      if (thursday === "true") {
+        storedDays.push(4);
+      }
+      if (friday === "true") {
+        storedDays.push(5);
+      }
+      if (saturday === "true") {
+        storedDays.push(6);
+      }
+      setSelectedDays(storedDays);
+    } catch (error) {
+      console.error("Error loading settings from storage", error);
+    }
   };
 
   const showTimepicker = () => {
@@ -65,30 +126,78 @@ const Home: React.FC = () => {
     });
   };
 
+  const configureAudio = async () => {
+    await Audio.setAudioModeAsync({
+      staysActiveInBackground: true,
+      /* interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX as any, */
+      playsInSilentModeIOS: true,
+      shouldDuckAndroid: false,
+      /* interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX as any, */
+    });
+  };
+
+  const handleAppStateChange = async (nextAppState: string) => {
+    if (nextAppState === "background" || nextAppState === "inactive") {
+      await Audio.setAudioModeAsync({
+        staysActiveInBackground: true,
+      });
+    }
+  };
+
   const increaseFrequency = () => {
     if (frequency < 60) {
       setFrequency(frequency + 1);
     }
   };
-  
+
   const decreaseFrequency = () => {
     if (frequency > 1) {
       setFrequency(frequency - 1);
     }
   };
+  
+  useEffect(() => {
+    loadSettingsFromStorage();
+    configureAudio();
+
+    const subscription = AppState.addEventListener('change', handleAppStateChange);
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
 
   useEffect(() => {
     if (startAnnouncingTime === true) {
-      // Announce the time immediately
       announceTime();
-
-      // Set an interval to announce the time every minute
       const intervalId = setInterval(announceTime, frequency * 60000);
-
-      // Clean up the interval when the component is unmounted
       return () => clearInterval(intervalId);
     }
   }, [frequency, startAnnouncingTime]);
+  
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date();
+      const currentDay = now.getDay();
+      const currentTime = now.getHours() * 60 + now.getMinutes();
+      // getDay returns a number between 0 and 6, where 0 is Sunday and 6 is Saturday
+      
+      console.log("selectedDays", selectedDays);
+      console.log("currentDay", currentDay);
+      console.log("currentTime", currentTime);
+      console.log("time", time);
+
+      if (selectedDays.includes(currentDay) && time) {
+        const selectedTimeInMinutes = time.getHours() * 60 + time.getMinutes();
+        if (currentTime === selectedTimeInMinutes) {
+          announceTime();
+          setStartAnnouncingTime(true);
+        }
+      }
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+  }, [selectedDays, time]);
 
   return (
     <View style={styles.container}>
@@ -96,7 +205,11 @@ const Home: React.FC = () => {
       <View style={styles.buttonsContainer}>
         <Button
           variant="primary"
-          text={startAnnouncingTime === true ? translate("pause") : translate("start")}
+          text={
+            startAnnouncingTime === true
+              ? translate("pause")
+              : translate("start")
+          }
           icon={startAnnouncingTime === true ? "pause" : "play"}
           leftIcon={true}
           iconSize={32}
@@ -120,31 +233,31 @@ const Home: React.FC = () => {
           onClickHandler={() => decreaseFrequency()}
           disabled={frequency > 1 ? false : true}
         />
-        {/* <Button
+        <Button
           variant="warning"
           text={translate("Select Time")}
           icon="clock-o"
           leftIcon={true}
           iconSize={32}
           onClickHandler={() => showTimepicker()}
-        /> */}
+        />
       </View>
 
-      {/* <View style={styles.daySelectorContainer}>
+      <View style={styles.daySelectorContainer}>
         <DayOfWeekSelector />
-      </View> */}
+      </View>
 
       <View style={styles.textContainer}>
         <Text style={styles.text}>
           {translate("frequency")}:{" "}
           {`${translate("every")} ${frequency} ${translate("minutes")}`}
         </Text>
-        {/* <Text style={styles.text}>
+        <Text style={styles.text}>
           {translate("selectedTime")}:{" "}
           {time === null
             ? translate("pleaseSelect")
             : time?.toLocaleTimeString()}
-        </Text> */}
+        </Text>
       </View>
 
       {show && (
--- components/DaySelector.tsx
+++ components/DaySelector.tsx
@@ -19,26 +19,56 @@ const DayOfWeekSelector: React.FC = () => {
   ];
   const [selectedDays, setSelectedDays] = useState<string[]>([]);
 
-  const toggleDay = (day: string) => {
+  const toggleDay = async (day: string, index: number) => {
+    let dayString: string;
+    switch (index) {
+      case 0:
+        dayString = "sunday";
+        break;
+      case 1:
+        dayString = "monday";
+        break;
+      case 2:
+        dayString = "tuesday";
+        break;
+      case 3:
+        dayString = "wednesday";
+        break;
+      case 4:
+        dayString = "thursday";
+        break;
+      case 5:
+        dayString = "friday";
+        break;
+      case 6:
+        dayString = "saturday";
+        break;
+      default:
+        dayString = "";
+        break;
+    }
+
     if (selectedDays.includes(day)) {
       setSelectedDays(
         selectedDays.filter((selectedDay) => selectedDay !== day)
       );
+      await AsyncStorage.setItem(dayString, "false");
     } else {
       setSelectedDays([...selectedDays, day]);
+      await AsyncStorage.setItem(dayString, "true");
     }
   };
 
   return (
     <View style={styles.viewContainer}>
-      {daysOfWeek.map((day) => (
+      {daysOfWeek.map((day, index) => (
         <TouchableOpacity
           style={[
             styles.inactiveButton,
             selectedDays.includes(day) ? styles.activeButton : null,
           ]}
           key={day}
-          onPress={() => toggleDay(day)}
+          onPress={() => toggleDay(day, index)}
         >
           <Text style={{ fontSize: fontSizes.general }}>{`${day[0]}`}</Text>
         </TouchableOpacity>
