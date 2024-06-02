9ee6caf6718d909c95c876403f24e2ba3a199e0f
Frank <frankedwards50@gmail.com>
2024-06-02 01:23:53 -0700

    Enabled InterruptionMode Do not mix android/ios

--- functions/page-specific/home/configureAudio.ts
+++ functions/page-specific/home/configureAudio.ts
@@ -4,10 +4,10 @@ import { Audio } from "expo-av";
 const configureAudio = async () => {
   await Audio.setAudioModeAsync({
     staysActiveInBackground: true,
-    /* interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX as any, */
+    interruptionModeIOS: 1,
     playsInSilentModeIOS: true,
     shouldDuckAndroid: false,
-    /* interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX as any, */
+    interruptionModeAndroid: 1,
   });
 };
 
